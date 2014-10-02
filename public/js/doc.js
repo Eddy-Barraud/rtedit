7b312a04d247532d51c8ba6212bc4697db2419d7
Viktor Qvarfordt <viktor.qvarfordt@gmail.com>
2014-10-02 16:53:45 +0200

    save mode between sessions and sync between users

--- public/js/doc.js
+++ public/js/doc.js
@@ -1,6 +1,6 @@
 /* global sharejs, io, ace, MathJax, Markdown */
 
-(function() {
+// (function() {
 
   var left = document.getElementById('left');
   var right = document.getElementById('right');
@@ -9,10 +9,12 @@
 
   var editor;
   var socket;
+  var state;
 
   var resize = function(r) {
     left.style.right = (1 - r) * 100 + '%';
     right.style.left = r * 100 + '%';
+    editor.resize();
   };
 
   var toggleDragHandle = function(state) {
@@ -38,7 +40,6 @@
 
         // Updates the preview with content from editor.
         return function() {
-          console.log('htmling');
           previewDoc.open();
           previewDoc.write(editor.getValue());
           previewDoc.close();
@@ -52,7 +53,6 @@
         inner.setAttribute('id', 'preview-text');
         el.appendChild(inner);
         return function() {
-          console.log('markdowning');
           inner.innerHTML = md2html(editor.getValue().replace(/\\\[/g, '\\\\[').replace(/\\\]/g, '\\\\]'));
           MathJax.Hub.Queue(['Typeset', MathJax.Hub, inner]);
         };
@@ -71,13 +71,14 @@
     editorSession.setTabSize(2);
     editorSession.setUseSoftTabs(true);
     editorSession.setUseWrapMode(true);
+    editor.resize();
     return editor;
   };
 
   var log = function(message, error) {
     if (error) {
-      console.error(message);
-      alert(message);
+      console.error('ERROR: ' + message);
+      alert('ERROR: ' + message);
     } else {
       console.log(message);
     }
@@ -97,8 +98,30 @@
         editor.setReadOnly(false);
         editor.moveCursorTo(0, 0);
         editor.focus();
+        editor.resize();
       }
       // document.getElementById('spinner').style.display = 'none';
+    });
+    sharejs.open(room + '\\meta', 'json', function(error, doc) {
+      if (error) {
+        log(error, true);
+      } else {
+        state = doc;
+        if (!state.get()) {
+          state.set({mode: 'text'});
+        }
+        document.getElementById('select-mode').value = state.at('mode').get();
+        selectMode(state.at('mode').get());
+        if (window.location.search.match(/hide-editor/) && left.style.right !== '0%') {
+          resize(0);
+        }
+        state.at().on('replace', function(path, was, now) {
+          if (was !== now) {
+            document.getElementById('select-mode').value = now;
+            selectMode(now);
+          }
+        });
+      }
     });
   };
 
@@ -110,310 +133,35 @@
   };
 
   var onEditorUpdate;
-  var onModeSelect = function(e) {
+  var selectMode = function(mode) {
     right.innerHTML = '';
     resize(1);
     toggleDragHandle(false);
-    editor.session.setMode('ace/mode/' + e.target.value);
-    if (rtedit.onMode[e.target.value]) {
+    editor.session.setMode('ace/mode/' + mode);
+    state.at('mode').set(mode);
+    if (rtedit.onMode[mode]) {
       editor.removeListener('change', onEditorUpdate);
-      onEditorUpdate = rtedit.onMode[e.target.value](right);
+      onEditorUpdate = rtedit.onMode[mode](right);
       editor.on('change', onEditorUpdate);
       onEditorUpdate();
     }
   };
 
   var initListeners = function() {
-    document.getElementById('select-mode').addEventListener('change', onModeSelect);
-    onModeSelect({target:document.getElementById('select-mode')}); // TODO: Save mode with socket.io
+    document.getElementById('select-mode').addEventListener('change', function(e) {selectMode(e.target.value);});
   };
 
   rtedit.init = function() {
     rtedit.editor = editor = initAce('editor');
-    initShare(window.location.pathname.split('/').pop());
     initListeners();
+    initShare(window.location.pathname.split('/').pop());
     initResize();
   };
 
-  // window.rtedit = {
-  //   previousFile: null,
-  //   visitedFiles: {}, // Restore cursor position when visiting an already visited file.
-  //   doc: null, // The current share.js document.
-  // };
 
-}());
+// }());
 
 window.rtedit.init();
-
-// Set up the ACE Editor
-
-
-// // Makes a smooth transition to a new document, by filename.
-// // (Position cursor at top, clear undo history, handle errors and things like that.)
-// function setDoc(filename) {
-//   if (!filename) return;
-//   if (doc && doc.name === filename) return;
-//   socket.emit('open', filename);
-//   editor.setReadOnly(true);
-//   document.title = filename;
-//   window.location.hash = filename;
-//   document.getElementById('filename').value = filename;
-//   sharejs.open(filenameEncode(filename), 'text', function(error, newDoc) {
-//     if (error) {
-//       console.error(error);
-//       alert(error);
-//       return;
-//     }
-//     if (doc !== null) {
-//       previousFile = filenameDecode(doc.name);
-//       visitedFiles[previousFile] = editor.getCursorPosition();
-//       doc.close();
-//       doc.detach_ace();
-//     }
-//     // Set editor mode based on file name extension.
-//     editor.session.setMode(modelist.getModeForPath(filename).mode);
-//     doc = newDoc;
-//     doc.attach_ace(editor);
-//     setTimeout(function() {
-//       if (visitedFiles[filename]) {
-//         editor.getSession().getSelection().moveCursorToPosition(visitedFiles[filename]);
-//       } else {
-//         editor.getSession().getSelection().moveCursorToPosition({row: 0, column: 0});
-//       }
-//       editor.clearSelection();
-//       editor.focus();
-//       editor.setReadOnly(false);
-//       editor.session.getUndoManager().reset();
-//     }, 0);
-//   });
-// }
-
-
-// // Pressing enter or ctrl+enter in the file selector opens that file.
-// document.getElementById('filename').addEventListener('keydown', function(e) {
-//   setTimeout(function() {
-//     if (e.keyCode === 13 && e.target.value) {
-//       if (e.ctrlKey) { // ctrl+enter go to exat file name (creates new file if file does not exist).
-//         setDoc(e.target.value);
-//       } else { // enter opens top suggestion, if one exists.
-//         var filename = filterFilenameSuggestions(e.target.value);
-//         if (filename) setDoc(filename);
-//       }
-//       filterFilenameSuggestions(null);
-//     } else {
-//       filterFilenameSuggestions(e.target.value);
-//     }
-//   }, 0);
-// });
-
-
-// // Save current file to disk by pressing ctrl+s.
-// window.addEventListener('keydown', function(e) {
-//   if (e.ctrlKey && e.keyCode === 83) { // ctrl+s
-//     if (doc) {
-//       socket.emit('save', {
-//         name: filenameDecode(doc.name),
-//         content: doc.getText()
-//       });
-//     }
-//     e.preventDefault();
-//   }
-// });
-
-
-// // Focus filename input with shortcut, for convenience this also clears the file selector.
-// window.addEventListener('keydown', function(e) {
-//   if (e.ctrlKey && e.keyCode === 81) { // ctrl+q
-//     document.getElementById('filename').value = '';
-//     document.getElementById('filename').focus();
-//     e.preventDefault();
-//     return false;
-//   }
-// });
-
-
-// // Open filename suggestions when focusing file selector.
-// document.getElementById('filename').addEventListener('focus', function(e) {
-//   filterFilenameSuggestions(e.target.value);
-// });
-
-
-// // Reset filename input if leaving the input without pressing enter.
-// document.getElementById('filename').addEventListener('blur', function(e) {
-//   e.target.value = doc ? filenameDecode(doc.name) : '';
-//   setTimeout(function() {
-//     filterFilenameSuggestions(null);
-//   }, 50); // Delay is needed so that the click event on each filename suggestion can go through. Unfortunately 0 ms is not sufficient, 50 ms is rather arbitrary; seems to be sufficient.
-// });
-
-
-// // Toggle previous file.
-// window.addEventListener('keydown', function(e) {
-//   if (e.altKey && e.keyCode === 81 && previousFile) { // alt+q
-//     e.preventDefault();
-//     setDoc(previousFile);
-//   }
-// });
-
-
-// // Change file from hash.
-
-// function setDocByHash() {
-//   if (window.location.hash) {
-//     setDoc(window.location.hash.substr(1));
-//   }
-// }
-
-// // Initially load file from hash.
-// setDocByHash();
-
-// // Listen to changes in hash.
-// window.addEventListener('hashchange', setDocByHash);
-
-
-// // Listen to whats going on.
-
-// // Display log message in log output for 3 seconds.
-// var lastLogMessageTimeout;
-// socket.on('log', function(msg) {
-//   console.log(msg);
-//   document.getElementById('log').textContent = msg;
-//   clearTimeout(lastLogMessageTimeout);
-//   lastLogMessageTimeout = setTimeout(function() {
-//     document.getElementById('log').textContent = '';
-//   }, 3000);
-// });
-
-// socket.on('error', function(msg) {
-//   console.error(msg);
-//   alert(msg);
-// });
-
-
-// // Synchronize state
-
-// var state = {
-//   existingFiles: [],
-//   userCount: 0
-// };
-
-// socket.on('update', function(data) {
-//   for (var property in data) {
-//     if (data.hasOwnProperty(property)) {
-//       state[property] = data[property];
-//       if (property === 'userCount') {
-//         document.getElementById('user-count').textContent = data[property];
-//       } else if (property === 'existingFiles' && !doc) {
-//         // Focus file selector at launch.
-//         document.getElementById('filename').focus();
-//         filterFilenameSuggestions('');
-//       }
-//     }
-//   }
-// });
-
-
-// // File name suggestions.
-
-// var filenameSuggestionsElement = document.getElementById('filename-suggestions');
-// function filterFilenameSuggestions(partialFilename) {
-//   filenameSuggestionsElement.innerHTML = '';
-//   if (partialFilename === null) return;
-//   var suggestions = [];
-//   if (partialFilename === '' ) {
-//     suggestions = state.existingFiles.map(function(e) {return [e, []];});
-//   } else {
-//     var regex = new RegExp(partialFilename.split('').map(function(c) {return '('+c+')';}).join('.*?'), 'i');
-//     state.existingFiles.forEach(function(existingFile) {
-//       var match = existingFile.match(regex);
-//       if (match && match[1]) {
-//         suggestions.push([existingFile, match.splice(1)]);
-//       }
-//     });
-//   }
-//   var metric = function(suggestion) {
-//     var n = 0;
-//     var i = 0;
-//     var prevIdx;
-//     suggestion[0].split('').forEach(function(char, idx) {
-//       if (char === suggestion[1][i]) {
-//         if (prevIdx)
-//           n += (idx - prevIdx);
-//         prevIdx = idx;
-//         i++;
-//       }
-//     });
-//     return n;
-//   };
-//   suggestions = suggestions.sort(function(a, b) {
-//     var n = metric(a) - metric(b);
-//     if (n === 0) return a[0].length - b[0].length;
-//     else return n;
-//   });
-//   suggestions.forEach(function(suggestion) {
-//     var displayHtml = '';
-//     var i = 0;
-//     suggestion[0].split('').forEach(function(char) {
-//       if (char === suggestion[1][i]) {
-//         displayHtml += '<strong>' + suggestion[1][i] + '</strong>';
-//         i++;
-//       } else {
-//         displayHtml += char;
-//       }
-//     });
-//     var child = document.createElement('div');
-//     child.innerHTML = displayHtml;
-//     filenameSuggestionsElement.appendChild(child);
-//     child.addEventListener('click', function(e) {
-//       setDoc(e.target.textContent);
-//       filterFilenameSuggestions(null);
-//     });
-//   });
-//   return suggestions[0] ? suggestions[0][0] : null;
-// }
-
-
-// // Since share.js does not allow slashes in the filename, we represent slashes by backslashes internally.
-
-// function filenameEncode(filename) {
-//   return filename.replace(/\//g, '\\');
-// }
-
-// function filenameDecode(filename) {
-//   return filename.replace(/\\/g, '/');
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var sharejs = require('share');
 
-// Set up http server (using express for serving static files)
-
 var app = express();
 var http = require('http').Server(app);
 var port = process.argv[3] || process.env.PORT || 8080;
@@ -10,52 +8,7 @@ var port = process.argv[3] || process.env.PORT || 8080;
 app.use('/', express.static(__dirname + '/public'));
 
 app.get('/:id', function(req, res) { res.sendFile(__dirname + '/public/doc.html'); });
-// app.get('*',  function(req, res) { res.redirect('/'); });
 
 http.listen(port, function() { console.log('Listening on http://localhost:' + port); });
 
-
-// Set up share.js
-
 sharejs.server.attach(app, {db: {type: 'none'}});
-// var connection = new sharejs.client.Connection('http://localhost:' + port + '/channel');
-
-
-// Set up socket.io
-
-/*
-var state = {
-};
-
-io.on('connection', function(socket) {
-
-  log(socket.request.connection.remoteAddress + ' connected');
-  io.emit('update', {userCount: ++state.userCount});
-
-  socket.emit('update', state);
-
-  // Once a client has connected, we expect to get a ping from them saying what room they want to join.
-  socket.on('room', function(room) {
-      socket.join(room);
-  });
-
-  socket.on('disconnect', function() {
-    log(socket.request.connection.remoteAddress + ' disconnected');
-    io.emit('update', {userCount: --state.userCount});
-  });
-
-});
-*/
-
-
-// Convenience functions
-
-function log(msg) {
-  console.log(msg);
-  io.emit('log', msg);
-}
-
-function logError(msg) {
-  console.error(msg);
-  io.emit('error', msg);
-}
